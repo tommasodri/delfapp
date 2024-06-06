a5f756bfec01ec0c1d8c28505dffe70c63408b1c
tommasodri <41899095+tommasodri@users.noreply.github.com>
2024-06-06 22:31:29 +0000

    complete app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,174 @@
-import streamlit as st 
+import streamlit as st
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
+import numpy as np
+import seaborn as sns
 
-st.balloons()
-st.markdown("# Data Evaluation App")
-
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
-
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
-
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
+# Dati delle posizioni
+posizioni = {
+    "Alice C":   [12, 13,  9,  4,  2,  6,  4,  3,  6,  4,  7,  5, 12],
+    "Alice P":   [ 3,  8, 13, None,  7, None,  5,  9, None, 13,  8, None, None],
+    "Assia":     [ 9,  5,  4,  3,  5,  5,  7, 12, 11, 11,  5,  7,  5],
+    "Aurora":    [14,  3, 11,  6,  8,  4, 12, None, 10,  6, None,  6,  9],
+    "Chiara":    [11, 10, 12, 11, 15, None,  2,  2, None, None, None, 11, None],
+    "Delfina":   [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
+    "Diletta":   [ 4,  6,  2,  2, 12,  2, None,  5,  2,  3, 10,  8,  3],
+    "Elena":     [13, 12,  7, None,  9, None,  9, 14,  3, 10, None,  9,  7],
+    "Eleonora":  [ 8,  2,  3, 13,  6,  3, 11,  4, 12,  2,  9, 10, 11],
+    "Elisa C":   [ 5,  8,  5,  8, 10,  7, 10, 11,  4,  7,  3,  3,  4],
+    "Elisa M":   [ 2,  4,  6, 10, 14, 10, None, 13, None, 12, None, None,  6],
+    "Marta":     [15,  9, None,  5,  4,  8,  3, 10,  9, 14,  2, None,  8],
+    "Silvia":    [10,  7,  8, 12, 11, None,  8,  8,  8,  9,  4, None, None],
+    "Valentina": [ 6, 14, 10,  9, 13,  9,  6,  7,  7,  8,  6,  2, 10],
+    "Zaira":     [ 7, 11, None,  7,  3, 11, 13,  6,  5,  5, None,  4,  2],
 }
 
-df = pd.DataFrame(data)
+colori_nodi = {
+    "Alice C": "#800000",        # bordeaux
+    "Alice P": "#228B22",        # verde bosco
+    "Assia": "#FFD700",          # oro
+    "Aurora": "#FFB6C1",         # rosa chiaro
+    "Chiara": "#C0C0C0",         # argento
+    "Delfina": "#FFFFFF",        # bianco
+    "Diletta": "#D40078",        # rosa ciclamino
+    "Elena": "#0000FF",          # blu
+    "Eleonora": "#191970",       # blu notte
+    "Elisa C": "#8A2BE2",        # viola
+    "Elisa M": "#FFA500",        # arancione
+    "Marta": "#87CEEB",          # celeste
+    "Silvia": "#FF0000",         # rosso
+    "Valentina": "#008000",      # verde
+    "Zaira": "#FF69B4",          # rosa
+}
 
-st.write(df)
+partecipanti = list(posizioni.keys())
+n_incontri = 13
+incontri = range(n_incontri)
 
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
+# Funzione per calcolare la distanza in un cerchio
+def distanza_cerchio(pos1, pos2, N):
+    return min(abs(pos1 - pos2), N - abs(pos1 - pos2))
 
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
+def calcola_matrice_vicinanza(incontro):
+    posizioni_presenti = [posizioni[p][incontro] for p in partecipanti if posizioni[p][incontro] is not None]
+    N = max(posizioni_presenti)
+    matrice_vicinanza = pd.DataFrame(np.zeros((len(partecipanti), len(partecipanti))), index=partecipanti, columns=partecipanti)
+    for i, p1 in enumerate(partecipanti):
+        for j, p2 in enumerate(partecipanti):
+            if posizioni[p1][incontro] is not None and posizioni[p2][incontro] is not None:
+                matrice_vicinanza.loc[p1, p2] = distanza_cerchio(posizioni[p1][incontro], posizioni[p2][incontro], N)
+    return matrice_vicinanza
 
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
-)
+# Funzione per calcolare la matrice di vicinanza media
+def calcola_matrice_vicinanza_media():
+    matrici = []
+    for incontro in incontri:
+        matrici.append(calcola_matrice_vicinanza(incontro))
+    matrice_vicinanza_media = sum(matrici) / len(matrici)
+    return matrice_vicinanza_media
 
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
+# Funzione per determinare se un colore è chiaro o scuro
+def is_color_light(color_hex):
+    color = color_hex.lstrip('#')
+    r, g, b = int(color[:2], 16), int(color[2:4], 16), int(color[4:], 16)
+    # Utilizzare la formula della luminanza percepita
+    luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+    return luminance > 0.5
+
+def print_heatmap(mask, fig, ax, matrice_vicinanza):
+    # Visualizza la matrice di vicinanza con una mappa di calore
+    ax = sns.heatmap(matrice_vicinanza, annot=True, fmt=".0f", cmap="RdYlGn_r", ax=ax, mask=mask, cbar_kws={"label": "Distanza"})
+
+    # Sposta le etichette dell'asse X in alto
+    ax.xaxis.set_ticks_position('top')
+    ax.xaxis.set_label_position('top')
+    plt.xticks(rotation=90)
+
+    st.pyplot(fig)
+
+def print_graph(G, fig, ax, pos, partecipanti_presenti):
+    # Aggiunta dei nodi
+    for partecipante in partecipanti_presenti:
+        G.add_node(partecipante, color=colori_nodi[partecipante])
+
+    # Disegno dei nodi con i colori specificati
+    node_colors = [nx.get_node_attributes(G, 'color')[n] for n in G.nodes()]
+    nx.draw_networkx_nodes(G, pos, node_size=2000, node_color=node_colors, ax=ax)
+
+    # Draw edges with colors based on weights
+    edges = G.edges(data=True)
+    weights = np.array([d['weight'] for u, v, d in edges])
+    norm_weights = (weights - weights.min()) / (weights.max() - weights.min())
+    edge_colors = [plt.cm.RdYlGn_r(1 - norm_weight) for norm_weight in norm_weights]
+
+    for (u, v, d), color in zip(edges, edge_colors):
+        nx.draw_networkx_edges(G, pos, edgelist=[(u, v)], width=d['weight'], edge_color=[color], ax=ax)
+
+    # Draw labels with contrasting colors
+    for node, (x, y) in pos.items():
+        node_color = colori_nodi[node]
+        color = 'black' if is_color_light(node_color) else 'white'
+        ax.text(x, y, node, fontsize=10, ha='center', va='center', color=color, fontweight='bold')
+
+    st.pyplot(fig)
+
+# Configurazione dell'app Streamlit
+st.title("Analisi delle vicinanze nei vari incontri")
+incontro_selezionato = st.slider("Seleziona l'incontro", 1, n_incontri, 1) - 1
+tipo_grafo = st.selectbox("Seleziona il tipo di grafo", ["Heatmap", "Network Graph"])
+
+matrice_vicinanza = calcola_matrice_vicinanza(incontro_selezionato)
+
+if tipo_grafo == "Heatmap":
+    mask = matrice_vicinanza == 0
+    fig, ax = plt.subplots()
+    # plt.title(f'Matrice di vicinanza per l\'incontro {incontro_selezionato + 1}')
+    print_heatmap(mask, fig, ax, matrice_vicinanza)
+else:
+    G = nx.Graph()
+
+    partecipanti_presenti = [p for p in partecipanti if posizioni[p][incontro_selezionato] is not None]
+
+    for i in range(len(partecipanti_presenti)):
+        for j in range(i + 1, len(partecipanti_presenti)):
+            p1, p2 = partecipanti_presenti[i], partecipanti_presenti[j]
+            if matrice_vicinanza.loc[p1, p2] > 0:
+                G.add_edge(p1, p2, weight=matrice_vicinanza.loc[p1, p2])
+
+    pos = nx.spring_layout(G)
+    edge_weights = nx.get_edge_attributes(G, 'weight')
+
+    fig, ax = plt.subplots(figsize=(14, 10))
+
+    print_graph(G, fig, ax, pos, partecipanti_presenti)
 
 st.divider()
 
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
+st.title("Analisi delle vicinanze medie su tutti gli incontri")
 
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
+matrice_vicinanza_media = calcola_matrice_vicinanza_media()
 
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
+st.subheader("Heatmap delle distanze medie")
+mask = matrice_vicinanza_media == 0
+fig, ax = plt.subplots()
+plt.title('Matrice di vicinanza media')
+print_heatmap(mask, fig, ax, matrice_vicinanza_media)
 
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
+st.subheader("Grafo delle distanze medie")
+G = nx.Graph()
 
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
+partecipanti_presenti = [p for p in partecipanti if any(posizioni[p])]
 
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
+for i in range(len(partecipanti_presenti)):
+    for j in range(i + 1, len(partecipanti_presenti)):
+        p1, p2 = partecipanti_presenti[i], partecipanti_presenti[j]
+        if matrice_vicinanza_media.loc[p1, p2] > 0:
+            G.add_edge(p1, p2, weight=matrice_vicinanza_media.loc[p1, p2])
 
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
+pos = nx.spring_layout(G)
+edge_weights = nx.get_edge_attributes(G, 'weight')
 
-st.bar_chart(df_plot, x = 'Category', y = 'count')
+fig, ax = plt.subplots(figsize=(14, 10))
 
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
-
+print_graph(G, fig, ax, pos, partecipanti_presenti)
